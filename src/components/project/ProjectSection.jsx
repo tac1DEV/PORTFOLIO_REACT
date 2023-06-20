f2dafe5a31f86c0904a2a65d15b7873c5eeae5ca
tacosougrecDEV <103994693+tacosougrecDEV@users.noreply.github.com>
2023-06-20 14:56:57 +0200

    Add files via upload

---
+++ src/components/project/ProjectSection.jsx
@@ -0,0 +1,30 @@
+import { SectionWrapper } from '../atom/SectionWrapper';
+import { Project } from './Project';
+import { getListOfUrlRepositoriesUrl } from '../../lib/api-url';
+import { Loader } from '../atom/Loader/Loader';
+import { useFetch } from '../../hooks/useFetch';
+import { GITHUB_USERNAME } from './../../lib/config';
+import {data} from '../../hooks/useFetch'
+
+
+export const ProjectSection = () => {
+  const {status, data: projects, error} = useFetch(getListOfUrlRepositoriesUrl(GITHUB_USERNAME));
+  if(status === "pending" || status === "idle"){
+    return <Loader/>
+  }
+
+  console.log(data);
+  if(error){
+    return <p>Error !</p>
+  }
+
+  return (
+    <SectionWrapper title="Projects">
+      <div className="flex flex-wrap justify-center gap-8">
+        {projects?.map(repository =>{
+          return <Project key={repository.name} {...repository}/>
+        })}
+      </div>
+    </SectionWrapper>
+  );
+};
