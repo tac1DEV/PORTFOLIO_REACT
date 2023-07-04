7ad784e89543f84d24f6be94f083ec352d5e8c11
tacosougrecDEV <103994693+tacosougrecDEV@users.noreply.github.com>
2023-07-05 01:04:43 +0200

    Update DrawSection.jsx
    .

--- src/components/draw/DrawSection.jsx
+++ src/components/draw/DrawSection.jsx
@@ -3,7 +3,7 @@ import { Draw } from './Draw';
 
 export const DrawSection = () => {
   return (
-    <SectionWrapper title="Montre moi ton amour !">
+    <SectionWrapper className="draw" title="Montre moi ton amour !">
       <Draw />
     </SectionWrapper>
   );
