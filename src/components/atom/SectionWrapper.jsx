7cd0deecbb10dcf629aa7767d793460b034ff8d4
tacosougrecDEV <103994693+tacosougrecDEV@users.noreply.github.com>
2023-07-04 23:32:12 +0200

    Update SectionWrapper.jsx
    removing draw section for mobile devices

--- src/components/atom/SectionWrapper.jsx
+++ src/components/atom/SectionWrapper.jsx
@@ -10,7 +10,7 @@ import { Typography } from "./Typography";
  */
 export const SectionWrapper = ({ children, title }) => {
   return (
-    <div className="flex flex-col items-center gap-12">
+    <div className="draw flex flex-col items-center gap-12">
       <Typography variant="h2">{title}</Typography>
       {children}
     </div>
