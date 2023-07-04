d4e27dcdd684b4c7af5da0873c5357d65de3717b
tacosougrecDEV <sansberrot@gmail.com>
2023-07-04 17:03:51 +0200

    Update config.js
    .

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,15 +8,4 @@ export const PORTFOLIO = 'Portfolio.';
 
 export const FULL_NAME = 'Thomas SANSBERRO';
 
-export const SOCIAL_NETWORKS = [
-  {
-    url: 'https://www.linkedin.com/in/thomas-sansberro-163682248/',
-    name: 'Linkedin',
-    icon: <SiLinkedin />,
-  },
-  {
-    url: 'https://github.com/tacosougrecDEV',
-    name: 'Github',
-    icon: <SiGithub />,
-  },
-];
+export const SOCIAL_NETWORKS = [{url: 'https://www.linkedin.com/in/thomas-sansberro-163682248/',name: 'Linkedin',icon: <SiLinkedin />,},{url: 'https://github.com/tacosougrecDEV',name: 'Github',icon: <SiGithub />,},];
